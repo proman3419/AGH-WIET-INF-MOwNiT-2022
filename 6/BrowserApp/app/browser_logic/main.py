f134455d89a30f9ace3156e054b3f1ab7e7cad6d
proman3419 <proman7268@gmail.com>
2022-05-03 23:58:53 +0200

    Added controls for noise reduction and requirements.txt

--- 6/BrowserApp/app/browser_logic/main.py
+++
@@ -1,8 +0,0 @@
-from WikipediaBrowserLogic import WikipediaBrowserLogic
-import numpy as np
-
-
-WBL = WikipediaBrowserLogic()
-WBL.fit(_load_dumped=True)
-# WBL.search(['war'], 5, print_text=False)
-WBL.search_raw(['water'], 10, noise_reduction=True, noise_reduction_value=0.3)
--- 6/BrowserApp/app/routes.py
+++ 6/BrowserApp/app/routes.py
@@ -4,7 +4,7 @@ from app.browser_logic.WikipediaBrowserLogic import WikipediaBrowserLogic
 
 
 WBL = WikipediaBrowserLogic()
-WBL.fit(_load_dumped=False)
+WBL.fit(_load_dumped=True)
 
 
 @app.route('/')
@@ -14,11 +14,13 @@ def input_form():
 
 @app.route('/', methods=['POST'])
 def input_form_post():
-    query = request.form['query-input-field']
-    results_cnt = request.form['resultscnt-input-field']
-    search_results = WBL.search_raw(query.split(), int(results_cnt), 
-                                    noise_reduction=True, 
-                                    noise_reduction_value=0.05)
+    query = request.form['query-if']
+    results_cnt = int(request.form['resultscnt-if'])
+    noise_reduction = bool(request.form['noise-reduction-cb'])
+    noise_reduction_value = float(request.form['noise-reduction-if'])
+    search_results = WBL.search_raw(query.split(), results_cnt, 
+                                    noise_reduction=noise_reduction, 
+                                    noise_reduction_value=noise_reduction_value)
 
     return render_template('results.html', query=query, results_cnt=results_cnt,
                            search_results=search_results)

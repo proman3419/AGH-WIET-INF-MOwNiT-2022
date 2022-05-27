f8e439752c45c15bad2accfec455958fc85958a8
proman3419 <proman7268@gmail.com>
2022-05-27 22:33:29 +0200

    lab9 base

--- 9/rotation_attempt.py
+++ 9/rotation_attempt.py
@@ -36,12 +36,12 @@ def show_steps(im_processed, pattern_processed, im_dft, figsize):
     im_dft = unswap(im_dft)
 
     fig, axs = plt.subplots(2, 2, figsize=figsize)
-    axs[0][0].imshow(im_processed, cmap='gray')
-    axs[0][0].set_title('Przetworzony obraz')
-    axs[0][1].imshow(pattern_processed, cmap='gray')
-    axs[0][1].set_title('Przetworzony wzorzec')
-    axs[1][0].imshow(np.log(abs(im_dft)), cmap='gray')
-    axs[1][0].set_title('Amplituda')
+    axs[0,0].imshow(im_processed, cmap='gray')
+    axs[0,0].set_title('Przetworzony obraz')
+    axs[0,1].imshow(pattern_processed, cmap='gray')
+    axs[0,1].set_title('Przetworzony wzorzec')
+    axs[1,0].imshow(np.log(abs(im_dft)), cmap='gray')
+    axs[1,0].set_title('Amplituda')
     axs[1,1].imshow(np.angle(im_dft), cmap='gray')
     axs[1,1].set_title('Faza')
     plt.show()
@@ -57,7 +57,7 @@ class MatchCandidate:
     x0: int
     y0: int
 
-def ocr(im_path, font_name, font_size):
+def ocr(im_path, font_name, font_size, C_thresh=1e-5):
     im, W, H = open_image(im_path)
     im, W, H = rotate_image(np.asarray(im))
     im, W, H = crop_text(np.asarray(im))
@@ -77,11 +77,10 @@ def ocr(im_path, font_name, font_size):
 
         max_pattern_C = np.max(np.real(ifft2(fft2(pattern_processed) * fft2(np.rot90(pattern_processed, k=2)))))
         C = np.abs(np.real(ifft2(im_dft * fft2(np.rot90(pattern_processed, k=2), s=[W, H]))) / max_pattern_C - 1)
-        C_err = 0.0001
 
         for x in range(W):
             for y in range(H):
-                if C[x][y] < C_err:
+                if C[x][y] < C_thresh:
                     x0, y0 = x - w + 1, y - h + 1
                     match_candidates.append(MatchCandidate(char, C[x][y], x0, y0))
 
@@ -116,7 +115,6 @@ def ocr_call_unwrapper(im_path):
     parts = im_path.split('/')[-1].split('_')
     font_name = parts[0] + '.ttf'
     font_size = int(parts[2])
-    # font_size = 18
     ocr(im_path, font_name, font_size)
 
 def rotate_image(im):
@@ -136,17 +134,16 @@ def rotate_image(im):
 
 # https://stackoverflow.com/questions/72202507/how-to-crop-image-to-only-text-section-with-python-opencv
 def crop_text(im):
-    img = im
     # Read in the image and convert to grayscale
-    img = img[:-20, :-20]  # Perform pre-cropping
-    gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    im = im[:-20, :-20]  # Perform pre-cropping
+    gray = cv2.cvtColor(im, cv2.COLOR_BGR2GRAY)
     gray = 255*(gray < 180).astype(np.uint8)  # To invert the text to white
     gray = cv2.morphologyEx(gray, cv2.MORPH_OPEN, np.ones(
         (2, 2), dtype=np.uint8))  # Perform noise filtering
     coords = cv2.findNonZero(gray)  # Find all non-zero points (text)
     x, y, w, h = cv2.boundingRect(coords)  # Find minimum spanning bounding box
     # Crop the image - note we do this on the original image
-    rect = img[y-3:y+h+1, x-2:x+w+1]
+    rect = im[y-3:y+h+1, x-2:x+w+1]
     out = Image.fromarray(np.uint8(rect))
     w, h = out.size
     return out, w, h
